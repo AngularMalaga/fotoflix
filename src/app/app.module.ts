cc900dc4a93c126438c371062cdd2720a7614e5b
Pablo Ortuño <pablo@willimbu.com>
2017-09-08 22:18:43 +0200

    added navbar and footer component adder angular 4 router test stuff is in /test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
-import { ImagenesService } from "app/imagenes.service";
+import { ImagenesService } from 'app/imagenes.service';
 import { PruebaServicioImagenesComponent } from './prueba-servicio-imagenes/prueba-servicio-imagenes.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { DiscoverComponent } from './pages/discover/discover.component';
+
+const appRoutes: Routes = [
+  { path: 'test', component: PruebaServicioImagenesComponent },
+  { path: 'item/:id', component: DiscoverComponent },
+  { path: '**', component: DiscoverComponent }
+
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    PruebaServicioImagenesComponent
+    PruebaServicioImagenesComponent,
+    NavbarComponent,
+    FooterComponent,
+    DiscoverComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [ImagenesService],
   bootstrap: [AppComponent]
